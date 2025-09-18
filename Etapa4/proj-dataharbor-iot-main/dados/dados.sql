6beb465b5e441f55150f1916020ac8c61a542378
Antonio Almeida - CID S320 Z981 <alfecjo@hotmail.com>
2025-09-18 20:37:10 -0300

    Update dados.sql

--- Etapa4/proj-dataharbor-iot-main/dados/dados.sql
+++ Etapa4/proj-dataharbor-iot-main/dados/dados.sql
@@ -32,5 +32,5 @@ CREATE TABLE IF NOT EXISTS users (
 -- Inserir os dados na tabela users
 INSERT INTO users (id, login, password, role, firstname, lastname, email, mobile, street, city, state, country)
 VALUES
-('98fda3da-a527-4d8b-89f2-9e59dd1749f7', 'acfa@acfa.com', '$2a$10$OVKiMx1bdq1Pud/rEga69O00MNmmEJ96u2DziVh13rTTcfuVFaF8y', '0', 'ACFA', 'de Almeida', 'acfa@acfa.br', '11934130986', 'Av Cel Sezefredo Fagundes, 20995', 'São Paulo', 'SP', 'Brasil'),
-('3297efde-b161-4190-a0f3-3d8eebecf5c6', 'jcfa@jcfa.com', '$2a$10$/k205Hwg/bNOrfC9i1VlaeIJnTQS.XwJ1e.o5j9blDoWFPg29N0oK', '1', 'Antonio', 'de Almeida', 'jcfa@jcfa.com', '11934130986', 'Av Cel Sezefredo Fagundes, 20995', 'São Paulo', 'SP', 'Brasil');
+('98fda3da-a527-4d8b-89f2-9e59dd1749f7', 'acfa@acfa.com', '$2a$10$OVKiMx1bdq1Pud/rEga69O00MNmmEJ96u2DziVh13rTTcfuVFaF8y', '0', 'ACFA', 'de Almeida', 'acfa@acfa.br', '11934130986', 'Av Sezefredo, 995', 'São Paulo', 'SP', 'Brasil'),
+('3297efde-b161-4190-a0f3-3d8eebecf5c6', 'jcfa@jcfa.com', '$2a$10$/k205Hwg/bNOrfC9i1VlaeIJnTQS.XwJ1e.o5j9blDoWFPg29N0oK', '1', 'ACFA', 'de Almeida', 'jcfa@jcfa.com', '11934130986', 'Av Sezefredo, 995', 'São Paulo', 'SP', 'Brasil');
